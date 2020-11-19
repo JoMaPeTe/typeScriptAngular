8f6af132ccbed526955c412798da5b9f74a4ebf4
JoseMPeTe <jmperezterrero@gmail.com>
2020-11-19 23:08:14 +0100

    Inicio a Typescript

---
+++ typscript/apps1.js
@@ -0,0 +1,17 @@
+"use strict";
+console.log('hola mundo');
+// tipos de datos en TypeScript
+//string
+var texto;
+texto = "hola soy un texto";
+//number
+var numero = 1234;
+//boolean
+var verdadFalso = false;
+//any
+var caulquiera = "cualquier cosa";
+// string[]
+var arrayCaracter = ["texto1", "texto2"];
+var arrayNum = [1, 2, 3];
+var arrayBoolean = [false, true];
+var arrayCualquiera = ["texto", 2, false];
---
+++ typscript/apps1.ts
@@ -0,0 +1,26 @@
+console.log('hola mundo');
+// tipos de datos en TypeScript
+
+//string
+
+let texto: string;
+texto = "hola soy un texto";
+
+//number
+
+let numero:number= 1234;
+
+//boolean
+
+let verdadFalso:boolean = false; 
+
+//any
+
+let caulquiera:any = "cualquier cosa";
+
+// string[]
+
+let arrayCaracter:string[] = ["texto1","texto2"];
+let arrayNum:number[] = [1,2,3];
+let arrayBoolean:boolean[] = [false, true];
+let arrayCualquiera:any[] = ["texto",2,false];
