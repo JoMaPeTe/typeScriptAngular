3df15b839280d3875cc2a13464c6e5adc85d1755
JoseMPeTe <jmperezterrero@gmail.com>
2020-12-02 17:50:53 +0100

    bootstrap ngIf else

---
+++ 3.-AngularIntro/my-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title:string = 'A mi primera aplicación';
+  show:boolean = true;
+}
