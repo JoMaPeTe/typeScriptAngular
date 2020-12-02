bea0c6bad53623b5e3c3779d28fb0b8b5d780f03
JoseMPeTe <jmperezterrero@gmail.com>
2020-12-02 21:54:28 +0100

    uso de ngFor

---
+++ 5.- ngFor/my-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  cursos:string[]=['HTML', 'ANGULAR', 'PHP']
+  animales: Array<any> = [
+    {tipo:'Perro', nombre:'Lisa', edad:10},
+    {tipo: 'Gato', nombre:'Pepon', edad:2},
+    {tipo: 'Pato', nombre: 'Tantan', edad:7}
+  ]
+}
