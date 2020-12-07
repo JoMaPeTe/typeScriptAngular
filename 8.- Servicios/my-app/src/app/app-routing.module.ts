98c36fe799131c9d331a803e61cbb0f4d536db78
JoseM <josmanpeter77@gmail.com>
2020-12-07 23:58:48 +0100

    Generamos el componente equipo, lo importamos a app-routing y el path con un id en route(antes de **) En nosotros.module.html en el href pasamos el id (que es el indice del objeto) Para ello añadimos index as i en ngfor, y en el ancla sustiruimos href por routerLink=['/equipo',i] Ahora tenemos que hacer que lea la ruta. En equipo.component.ts importamos activated Route, creamos una variable privada de la clase ActivatedRoute en el constructor, y le aplicamos los métodos params y subscribe para obtener el valor que nos interesa de esa ruta. Volvemos al servicio, donde creamos la funcion obtenerUno. Esta funcion del servicio queremos usarla en equipo.component, así que importamos el servicio, y en el constructor creamos una variable privada para trabajar con el. Además creamos la variable equipo[] tipo any, a la que le pasamos el servicio, la funcions obtenerUno y el index. La información de la variable equipo:any= {} que ya tenemos en equipo.component.ts, la plasmamos en equipo.component.html

--- 8.- Servicios/my-app/src/app/app-routing.module.ts
+++ 8.- Servicios/my-app/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactoComponent } from './contacto/contacto.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { EquipoComponent } from './equipo/equipo.component';
 
 const routes: Routes = [
   {path:'contacto', component: ContactoComponent},
   {path:'nosotros', component: NosotrosComponent},
   {path:'inicio', component:InicioComponent},
+  {path:'equipo/:id', component: EquipoComponent},
   {path:'', component:InicioComponent, pathMatch: 'full'},
   { path: '**',   redirectTo: '/', pathMatch: 'full'},
+  
+  
 
 ];
 
